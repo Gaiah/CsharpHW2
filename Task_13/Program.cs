4ea853cde8e6c383b294fad872cf21dee00bd93b
Gaiah <gaiah.a.akopian@gmail.com>
2022-08-11 08:32:26 +0400

    Task_13 added

---
+++ Task_13/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Enter some number: ");
+int usersNumber = Convert.ToInt32(Console.ReadLine());
+int cycle = 0;
+
+while (cycle < usersNumber) {
+    if (usersNumber > 999) 
+    {
+        usersNumber = usersNumber / 10;
+    }
+    cycle++;
+}
+
+if (usersNumber > 99 && usersNumber < 1000) 
+{
+    Console.Write("The third digit is: ");
+    Console.WriteLine(usersNumber % 10);
+}
+
+else 
+{
+ Console.WriteLine("There's no third digit in your number.");
+}
