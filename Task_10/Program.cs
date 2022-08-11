5a8d5ba436e417f3eee4a9d54480ff6d25e72e3c
Gaiah <gaiah.a.akopian@gmail.com>
2022-08-11 04:51:27 +0400

    Task_10 added

---
+++ Task_10/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter a three-digit number");
+int usersNumber = Convert.ToInt32(Console.ReadLine());
+
+if (1000 > usersNumber && usersNumber > 99)
+{
+ Console.Write("The entered number's second digit is: ");
+ Console.WriteLine(usersNumber / 10 % 10);
+}
+
+else 
+{
+ Console.WriteLine("Entered number isn't a three-digit number.");
+}
