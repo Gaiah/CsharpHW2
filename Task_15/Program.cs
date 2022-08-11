e3b8365bfc2523ff5efc27e82cc241bb1286a120
Gaiah <gaiah.a.akopian@gmail.com>
2022-08-11 09:23:02 +0400

    Task_15 added

---
+++ Task_15/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter a number 1-7. A little hint bellow: Monday - 1; Tuesday - 2; Wednesday - 3; Thursday - 4; Friday - 5; Saturday - 6; Sunday - 7.");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day > 0 && day < 8) 
+{
+ if ( day == 6 || day == 7)
+ {
+  Console.WriteLine("Yaay! Enjoy Your weekend!");
+ }
+ else 
+ {
+  Console.Write("Sorry, it's not a weekend yet.");
+ }
+}
+
+else 
+{
+ Console.WriteLine("Incorrect number!");
+}
